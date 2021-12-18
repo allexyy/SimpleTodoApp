c1d7c8a7821644be02d843d8eb2c394937c882bc
Алексей Луканов <lukanov.a@roistat.com>
2021-12-18 15:30:49 +0300

    Initial Commit

---
+++ SImpleTodo/SImpleTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SImpleTodoApp.swift
+//  SImpleTodo
+//
+//  Created by Луканов Алексей on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SImpleTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
